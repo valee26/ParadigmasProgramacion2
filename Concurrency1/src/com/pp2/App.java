6161cae2a809d6b87039ee71dfb04209fdde71ae
valee26 <124370942+valee26@users.noreply.github.com>
2023-05-29 15:38:25 -0600

    Add files via upload

---
+++ Concurrency1/src/com/pp2/App.java
@@ -0,0 +1,14 @@
+package com.pp2;
+
+public class App {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+         Runner1 t1=new Runner1();
+         Runner2 t2=new Runner2();
+         
+         t1.start();
+         t2.start();
+	}
+
+}
