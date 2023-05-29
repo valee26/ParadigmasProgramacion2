60910138be525b61db41e9d6d8f681aa47b1a0c5
valee26 <124370942+valee26@users.noreply.github.com>
2023-05-29 15:37:54 -0600

    Add files via upload

---
+++ Mulithreading2/src/com/pp2/App.java
@@ -0,0 +1,13 @@
+package com.pp2;
+
+public class App {
+
+	public static void main(String[] args) {
+
+		Thread t1 = new Thread (new Runner1());
+		Thread t2 = new Thread (new Runner2());
+		t1.start();
+		t2.start();
+	}
+
+}
